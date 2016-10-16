1c8dc2a1c19159e0223f7b402dca5ad7cf9fe932
Cedric Stoquer <cstoquer@wizcorp.jp>
2016-10-16 22:39:06 +0900

    warp physic

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+var gameView    = require('./view/gameView');
 var level       = require('./Level');
 var Monkey      = require('./Monkey');
 var getGamepads = require('./gamepad.js');
@@ -10,34 +11,9 @@ var canvas = $screen.canvas;
 canvas.style.width  = '100%';
 canvas.style.height = '100%';
 
-//▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
-var levelNum = random(2) + 1;
-level.load(assets.levels['level' + levelNum]);
 
-// TODO: make this array dynamic to add and remove monkeys
-var monkeys = [
-	new Monkey(0),
-	new Monkey(1),
-	new Monkey(2),
-	new Monkey(3)
-];
-
-var spawnPoints = level.getSpawnPoints();
-for (var i = 0; i < monkeys.length; i++) {
-	monkeys[i].x = spawnPoints[i].x * TILE_WIDTH;
-	monkeys[i].y = spawnPoints[i].y * TILE_HEIGHT;
-}
-
-console.log(monkeys)
 
 //▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
 // Update is called once per frame
-exports.update = function () {
-	cls();
-	level.draw();
-	var gamepads = getGamepads();
-	for (var i = 0; i < monkeys.length; i++) {
-		monkeys[i].update(gamepads);
-		monkeys[i].draw();
-	}
-};
+gameView.open();
+exports.update = gameView.update;
